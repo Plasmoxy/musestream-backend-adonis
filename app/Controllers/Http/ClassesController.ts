157a451c6e17ba322f2fb0d3ab4d8e46f979e81b
ottonovak <xnovaks1@stuba.sk>
2022-03-29 11:47:47 +0200

    class controller

--- app/Controllers/Http/ClassesController.ts
+++ app/Controllers/Http/ClassesController.ts
@@ -2,6 +2,8 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { Exception } from '@adonisjs/core/build/standalone'
 import { schema } from '@ioc:Adonis/Core/Validator'
 import Class from 'App/Models/Class'
+import User from 'App/Models/User'
+import authConfig from 'Config/auth'
 
 export default class ClassesController {
   public async getClassesOfUser({ auth }: HttpContextContract) {
@@ -9,19 +11,25 @@ export default class ClassesController {
     return classes
   }
 
-  public async createNewClass({ request }: HttpContextContract) {
+  public async createNewClass({ request, auth }: HttpContextContract) {
     const body = await request.validate({
       schema: schema.create({
         title: schema.string(),
         description: schema.string(),
         instrument: schema.string(),
         difficulty: schema.string(),
-        teacherId: schema.number(),
       }),
     })
+    const newClass = await auth.user!.related('classes').create(body)
+    return newClass
   }
 
-  public async getClass({}: HttpContextContract) {}
+  public async getClass({ request }: HttpContextContract) {
+    const cls = await Class.find(request.param('id'))
+    if (cls == null) throw new Exception('Class not found', 404)
+
+    return cls
+  }
 
   public async updateClass({ request }: HttpContextContract) {
     const body = await request.validate({
@@ -33,6 +41,11 @@ export default class ClassesController {
         teacherId: schema.number(),
       }),
     })
+
+    const cls = await Class.find(request.param('id'))
+    if (cls === null) throw new Exception('User not found', 404)
+
+    return await cls.merge(body).save()
   }
 
   public async deleteClass({ request }: HttpContextContract) {
