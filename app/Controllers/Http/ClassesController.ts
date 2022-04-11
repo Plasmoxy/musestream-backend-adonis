d7696f0a0e15034bc58d2d543088f59bda5c7061
Plasmoxy <plasmoxy@gmail.com>
2022-04-11 14:05:32 +0200

    teacher preload

--- app/Controllers/Http/ClassesController.ts
+++ app/Controllers/Http/ClassesController.ts
@@ -6,18 +6,18 @@ import Class from 'App/Models/Class'
 export default class ClassesController {
   
   public async getAllClasses() {
-    return await Class.query()
+    return await Class.query().preload('teacher')
   }
   
   public async getClassesOfUser({ auth }: HttpContextContract) {
     // if its teacher, just get related classes
     if (auth.user!.type === 'teacher') {
-      return await auth.user!.related('teacherClasses').query()
+      return await auth.user!.related('teacherClasses').query().preload('teacher')
     }
     
     // if its student, get student classes (through class_student pivot)
     if (auth.user!.type === 'student') {
-      return await auth.user!.related('studentClasses').query()
+      return await auth.user!.related('studentClasses').query().preload('teacher')
     }
   }
 
@@ -30,12 +30,14 @@ export default class ClassesController {
       }),
     })
     const newClass = await auth.user!.related('teacherClasses').create(body)
+    await newClass.load('teacher')
     return newClass
   }
 
   public async getClass({ request }: HttpContextContract) {
     const cls = await Class.find(request.param('id'))
     if (cls === null) throw new Exception('Class not found', 404)
+    await cls.load('teacher')
     return cls
   }
 
@@ -51,8 +53,9 @@ export default class ClassesController {
     const cls = await Class.find(request.param('id'))
     if (cls === null) throw new Exception('Class not found', 404)
     if (cls.teacherId !== auth.user!.id) throw new Exception('You can edit only your classes', 400)
-    
-    return await cls.merge(body).save()
+    await cls.merge(body).save()
+    await cls.load('teacher')
+    return cls
   }
 
   public async deleteClass({ request, auth }: HttpContextContract) {
